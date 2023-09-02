94697e0e3f7f6d91eb3c3ab14a93a6e8440857bb
Sean Blizard <emailseanblizard@gmail.com>
2023-09-02 12:25:49 -0400

    main

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,30 +2,30 @@
 
 __author__ = "730642587"
 
-guessWord = input("Enter a 5-character word: ")
+guess_word = input("Enter a 5-character word: ")
 
-if len(guessWord) != 5:
+if len(guess_word) != 5:
     print("Error: word must be 5 characters long")
     exit()
 
-guessedCharacter = input("Enter a single character: ")
+guessed_character = input("Enter a single character: ")
 
-if  len(guessedCharacter) != 1:
+if  len(guessed_character) != 1:
     print("Error: character must be a single character")
     exit()
     
     
-print("Searching for " + guessedCharacter + " in " + guessWord)
+print("Searching for " + guessed_character + " in " + guess_word)
 
-charCount: int = 0
+char_count: int = 0
 
 for i in range(5):
-    if guessWord[i]==guessedCharacter:
-        charCount+=1
-        print(guessedCharacter + " found at index " + str((i)))
+    if guess_word[i]==guessed_character:
+        char_count+=1
+        print(guessed_character + " found at index " + str((i)))
 
-if charCount == 0:
-    print("No instances of " + guessedCharacter + " found in " + guessWord)
+if char_count == 0:
+    print("No instances of " + guessed_character + " found in " + guess_word)
 else:
-    print(str(charCount) + " instances of " + guessedCharacter + " found in " + guessWord)
+    print(str(char_count) + " instances of " + guessed_character + " found in " + guess_word)
 
