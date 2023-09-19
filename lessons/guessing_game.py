b2cd81f6f2217e58106a4e61cb12555a52abda8f
Sean Blizard <emailseanblizard@gmail.com>
2023-09-19 12:23:42 -0400

    mian

--- lessons/guessing_game.py
+++ lessons/guessing_game.py
@@ -7,7 +7,10 @@ secret: int = randint(1,10)
 guess: int = int(input("Guess a number between 1 and 10: "))
 
 while guess != secret:
-    print("Wrong!")
+    if guess < secret:
+        print("Too low")
+    else:
+        print("Too high")
     guess = int(input("Guess a number between 1 and 10: "))
 
 print(f"You got it! The correct number was {secret}.")
