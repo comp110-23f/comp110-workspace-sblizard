ecdef97bb6d44bf811bb10617317a37dfa1f3cb2
Sean Blizard <emailseanblizard@gmail.com>
2023-09-19 12:00:56 -0400

    mian

---
+++ lessons/guessing_game.py
@@ -0,0 +1,13 @@
+"""program that loops until correct number is guessed"""
+
+from random import randint
+
+secret: int = randint(1,10)
+
+guess: int = int(input("Guess a number between 1 and 10: "))
+
+while guess != secret:
+    print("Wrong!")
+    guess = int(input("Guess a number between 1 and 10: "))
+
+print(f"You got it! The correct number was {secret}.")
