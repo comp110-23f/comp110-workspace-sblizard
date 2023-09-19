8515a825ecf1ec8d2f409344406a2c12186fb3b6
Sean Blizard <emailseanblizard@gmail.com>
2023-09-19 10:29:28 -0400

    main

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -7,30 +7,42 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
+# input for user
 guess = input(f"What is your {len(word_to_guess)}-letter guess? ")
 
+# keeps making the user input until the lenght of the guess matches the length of the word to guess
 while len(guess) != 6:
-    guess = input("That was not 6 letters! Try again: ")
+    guess = input(f"That was not {len(word_to_guess)} letters! Try again: ")
 
+# initalizes variables to help with iteration and see if the answer is correct
 index = 0
-
-currentIndexEmoji = ""
-
+current_index_emoji = ""
 correct = True
 
+# iterate through each index of guess and compares to coresponding index of word_to_guess
 while index < len(word_to_guess):
     if word_to_guess[index] == guess[index]:
-        currentIndexEmoji = GREEN_BOX
-    elif guess[index] in word_to_guess:
-        currentIndexEmoji = YELLOW_BOX
-        correct = False
+        current_index_emoji = GREEN_BOX
     else:
-        currentIndexEmoji = WHITE_BOX
+        current_index_emoji = WHITE_BOX
         correct = False
+
+    # determines if the current index of guess exists somewhere else in word_to_guess that isnt the same index
+    exist_elsewhere = False
+    i = 0
+    while i < len(word_to_guess):
+        if (guess[index] == word_to_guess[i]) and (i != index):
+            exist_elsewhere = True
+        i += 1
+
+    if exist_elsewhere:
+        current_index_emoji = YELLOW_BOX
+
+    # prints out the correct emoji
     if index == len(word_to_guess) - 1:
-        print(f"{currentIndexEmoji}")
+        print(f"{current_index_emoji}")
     else:
-        print(f"{currentIndexEmoji}", end="")
+        print(f"{current_index_emoji}", end="")
     index += 1
 
 if correct:
