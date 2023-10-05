3dcc412059a8067b0b45f71032e9df92eed039c2
Sean Blizard <emailseanblizard@gmail.com>
2023-10-05 12:40:30 -0400

    main

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,11 +2,12 @@
 
 __author__ = "730624587"
 
-test_list: list[int] = [1,2,3,4]
 
 def all(iterate_list: list[int], element: int) -> bool:
-    '''Function that returns True if all of the elements in the given list are the same as the given element.'''
+    """Function that returns True if all of the elements in the given list are the same as the given element."""
     i: int = 0
+    if len(iterate_list) == 0:
+        return False
     while i < len(iterate_list):
         if (iterate_list[i] != element):
             return False
@@ -14,8 +15,8 @@ def all(iterate_list: list[int], element: int) -> bool:
     return True
 
 
-def max(iterate_list: list[int]):
-    '''Function that returns the max value in the given list.'''
+def max(iterate_list: list[int]) -> int:
+    """Function that returns the max value in the given list."""
     if len(iterate_list) == 0:
         raise ValueError("max() arg is an empty List")
     max_value: int = iterate_list[0]
@@ -26,8 +27,9 @@ def max(iterate_list: list[int]):
         i += 1
     return max_value
 
-def is_equal(list_one: list[int], list_two: list[int]):
-    '''Function that returns True if the two lists are the same.'''
+
+def is_equal(list_one: list[int], list_two: list[int]) -> bool:
+    """Function that returns True if the two lists are the same."""
     i: int = 0
     if len(list_one) == len(list_two):
         while i < len(list_two):
