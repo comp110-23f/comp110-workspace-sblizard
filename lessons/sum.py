8f699433f6fe2274a02224f17c442264e342857e
Sean Blizard <emailseanblizard@gmail.com>
2023-10-17 11:48:50 -0400

    main

--- lessons/sum.py
+++ lessons/sum.py
@@ -6,7 +6,7 @@ __author__ = "730642587"
 def w_sum(vals: list[float]) -> float:
     """Function that returns the sum of a list using a while loop."""
     i: int = 0
-    sum: float = 0
+    sum: float = 0.0
     while i < len(vals):
         sum += vals[i]
         i += 1
@@ -15,7 +15,7 @@ def w_sum(vals: list[float]) -> float:
 
 def f_sum(vals: list[float]) -> float:
     """Function that returns the sum of a list using a for loop."""
-    sum: float = 0
+    sum: float = 0.0
     for i in vals:
         sum += i
     return sum
@@ -23,7 +23,9 @@ def f_sum(vals: list[float]) -> float:
 
 def f_range_sum(vals: list[float]) -> float:
     """Function that returns the sum of a list using a for loop and range."""
-    sum: float = 0
+    sum: float = 0.0
     for i in range(0, len(vals)):
         sum += vals[i]
     return sum
+
+print(w_sum([]))
