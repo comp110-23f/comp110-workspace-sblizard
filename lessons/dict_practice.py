4339ff6edfa0054813cf08876ec94a0cbfd196b5
Sean Blizard <emailseanblizard@gmail.com>
2023-10-24 11:49:34 -0400

    main

---
+++ lessons/dict_practice.py
@@ -0,0 +1,15 @@
+"""Practice with creating a new dictionary"""
+ice_cream: dict[str, int] = {"chocalte": 12, "vanilla": 8, "strawberry": 5}
+
+ice_cream["mint"] = 3
+
+ice_cream.pop("mint")
+
+ice_cream["vanilla"] = 10
+
+print(ice_cream)
+
+
+
+for i in ice_cream:
+    print(i + "\n" + str(ice_cream[i]))
